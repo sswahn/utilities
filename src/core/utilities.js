f96ad39f4f22a12669adb57238f5e8a1e89750da
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-23 00:03:15 -0400

    Update utilities.js

--- src/core/utilities.js
+++ src/core/utilities.js
@@ -1,1 +1,25 @@
-//
+export const debounce = (fn, delay) => {
+  let timeoutId
+  return (...args) => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
+}
+
+export const throttle = (fn, limit) => {
+  let lastCall = 0
+  return (...args) => {
+    const now = Date.now()
+    if (now - lastCall < limit) {
+      return
+    }
+    lastCall = now
+    fn(...args)
+  }
+}
+
+export const deepClone = obj => {
+  return JSON.parse(JSON.stringify(obj))
+}
