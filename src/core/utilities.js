5b743bffe36a92646cad0472cb72ac3c186074ca
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 17:08:53 -0500

    Update utilities.js

--- src/core/utilities.js
+++ src/core/utilities.js
@@ -26,6 +26,19 @@ export const deepClone = obj => {
     : JSON.parse(JSON.stringify(obj))
 }
 
+export const deepMerge = (target, source) => {
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      if (source[key] instanceof Object && key in target) {
+        deepMerge(target[key], source[key])
+      } else {
+        target[key] = source[key]
+      }
+    }
+  }
+  return target
+}
+
 export const pipe = (...fns) => {
   return arg => fns.reduce((prev, fn) => fn(prev), arg)
 }
